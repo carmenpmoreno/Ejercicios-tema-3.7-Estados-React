0afa1bf0c900b3846ad04918425a739a0687c68e
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-10 18:15:33 +0200

    finish first exercise

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+    this.handleInputText = this.handleInputText.bind(this);
+  }
+
+  handleInputText(event) {
+    if (event.target.value) {
+      this.setState({
+        content: event.target.value
+      });
+    } else {
+      this.setState({
+        content: ''
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <label>Escribe aquí</label>
+        <input
+          type="text"
+          className="input-text"
+          id="input-text"
+          onChange={this.handleInputText}
+        />
+        <p>{this.state.content}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
