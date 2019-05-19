b88e56fd7074d8d869010cf749deabf694102c1e
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-19 22:34:52 +0200

    unfinished esvercise

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Reloj from './components/Reloj';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: ''
-    };
-    this.handleInputText = this.handleInputText.bind(this);
-  }
-
-  handleInputText(event) {
-    if (event.target.value) {
-      this.setState({
-        content: event.target.value
-      });
-    } else {
-      this.setState({
-        content: ''
-      });
-    }
-  }
-
   render() {
     return (
-      <div className="App">
-        <label>Escribe aquí</label>
-        <input
-          type="text"
-          className="input-text"
-          id="input-text"
-          onChange={this.handleInputText}
-        />
-        <p>{this.state.content}</p>
-      </div>
+      <Reloj/>
     );
   }
 }
---
+++ src/components/Reloj.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Reloj extends React.Component {
+
+    constructor(props) {
+        super(props);
+        setInterval(
+            updateClock(
+        
+            )
+        );
+    }    
+
+    updateClock(
+        this.setState((prevState)=>{})
+
+    )
+    render() {
+        return(
+            <div>{newDate}</div>
+        )
+    }
+}
+
+export default Reloj;
